799280edb5dcd387813846cc4cd97063cbf6547c
Marcelo Benicio <mbenicio19@gmail.com>
2021-10-03 23:07:50 -0300

    inclusao rds

--- 3_data_lakes/app.py
+++ 3_data_lakes/app.py
@@ -1,7 +1,7 @@
 
 from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
-# from data_platform.common_stack import CommonStack
+from data_platform.common_stack import CommonStack
 # from data_platform.dms.stack import DmsStack
 # from data_platform.kinesis.stack import KinesisStack
 # from data_platform.glue_catalog.stack import GlueCatalogStack
@@ -11,7 +11,7 @@ from data_platform.data_lake.stack import DataLakeStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
-# common_stack = CommonStack(app)
+common_stack = CommonStack(app)
 # dms = DmsStack(app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 # kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 # glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket=data_lake.data_lake_raw_bucket, processed_data_lake_bucket=data_lake.data_lake_processed_bucket)
